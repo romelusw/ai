8e80e44b4dc470f16776593084b11d4f002cd60a
Woody Romelus <romelus.w@gmail.com>
2015-10-22 15:46:03 -0400

    Adding board reading/rendering

--- wumpus/src/main/java/com/romelus_borucki/common/utils/WumpusBoardHelper.java
+++ wumpus/src/main/java/com/romelus_borucki/common/utils/WumpusBoardHelper.java
@@ -1,8 +1,12 @@
 package com.romelus_borucki.common.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,11 +19,12 @@ public class WumpusBoardHelper {
     /**
      * Pattern for board dimensions.
      */
-    private static final Pattern BOARD_DIMENSIONS_PATTERN = Pattern.compile("^Size.*(\\d),(\\d+)$");
+    private static final Pattern BOARD_DIMENSIONS_PATTERN = Pattern.compile("^Size.* (\\d+),(\\d+)$");
 
     public static void main(final String...args) {
         final ClassLoader classLoader = WumpusBoardHelper.class.getClassLoader();
-        readBoard(new File(classLoader.getResource("b1.txt").getFile()));
+        final BoardPiece[][] board = readBoard(new File(classLoader.getResource("b1.txt").getFile()));
+        printBoard(board);
     }
 
     /**
@@ -59,13 +64,56 @@ public class WumpusBoardHelper {
     }
 
     /**
+     * A class which represents a board piece for the wumpus world.
+     */
+    public static class BoardPiece {
+        private int x;
+        private int y;
+        private Set<PieceType> types = new HashSet<PieceType>();
+
+        /**
+         * Default constructor.
+         *
+         * @param xloc the x-position of the piece on the board
+         * @param yloc the y-position of the piece on the board
+         */
+        public BoardPiece(final int xloc, final int yloc) {
+            x = xloc;
+            y = yloc;
+        }
+
+        /**
+         * Adds a type to the piece.
+         *
+         * @param type the type of piece to add
+         * @return flag indicating if add successfully
+         */
+        public boolean addType(final PieceType type) {
+            return types.add(type);
+        }
+
+        /**
+         * Getter for types.
+         *
+         * @return the types
+         */
+        public String getTypes() {
+            final StringBuilder sb = new StringBuilder();
+            for(PieceType type : types) {
+                sb.append(type.literal);
+            }
+            return sb.toString();
+        }
+    }
+
+    /**
      * Reads a specially formatted file to representing a wumpus board.
      *
      * @param f the file
      * @return the resolved board
      */
-    public static String[][] readBoard(final File f) {
-        String[][] retVal = null;
+    public static BoardPiece[][] readBoard(final File f) {
+        BoardPiece[][] retVal = null;
         try {
             final Scanner s = new Scanner(f);
             int lineNum = 0;
@@ -74,7 +122,7 @@ public class WumpusBoardHelper {
                 if(lineNum == 0) {
                     final Matcher m = BOARD_DIMENSIONS_PATTERN.matcher(line);
                     if (m.find()) {
-                        retVal = new String[Integer.parseInt(m.group(0))][Integer.parseInt(m.group(1))];
+                        retVal = new BoardPiece[Integer.parseInt(m.group(1))][Integer.parseInt(m.group(2))];
                     } else {
                         break;
                     }
@@ -82,8 +130,12 @@ public class WumpusBoardHelper {
                     final String[] tokens = line.split(",");
                     final int x = Integer.parseInt(tokens[0]);
                     final int y = Integer.parseInt(tokens[1]);
-                    final PieceType bp = PieceType.fromString(tokens[2]);
-                    retVal[x][y] = bp.literal;
+                    final BoardPiece bp = new BoardPiece(x, y);
+                    // Add all the types
+                    for(int i = 2; i < tokens.length; i++) {
+                        bp.addType(PieceType.fromString(tokens[i]));
+                    }
+                    retVal[x][y] = bp;
                 }
                 lineNum++;
             }
@@ -98,14 +150,18 @@ public class WumpusBoardHelper {
      *
      * @param board the board
      */
-    public void printBoard(final String[][] board) {
+    public static void printBoard(final BoardPiece[][] board) {
         final int rows = board.length;
         final int cols = board[0].length;
-        final StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < rows; i++) {
+        final String tableHeader = new String(new char[cols]).replace("\0", "+---------") + "+\n";
+        final StringBuilder sb = new StringBuilder(tableHeader);
+        for(int i = rows - 1; i >= 0; i--) {
             for(int j = 0; j < cols; j++) {
-                sb.append("");
+                final String types = board[j][i].getTypes().toString();
+                sb.append("|")
+                        .append(StringUtils.center(String.format("  %s  ", types), 9));
             }
+            sb.append("|\n").append(tableHeader);
         }
         System.out.println(sb);
     }
--- wumpus/src/main/java/com/romelus_borucki/logicalAgents/InferenceAgent.java
+++ wumpus/src/main/java/com/romelus_borucki/logicalAgents/InferenceAgent.java
@@ -1,15 +1,51 @@
 package com.romelus_borucki.logicalAgents;
 
+import com.romelus_borucki.common.utils.WumpusBoardHelper.BoardPiece;
+
 /**
  * Created by romelus on 10/22/15.
  */
 public class InferenceAgent {
-//    var clockwise = d - c;
+    //    var clockwise = d - c;
 //    var cclockwise = (4 - clockwise) % 4;
 //    console.log("direction:", clockwise < cclockwise ? 1 : -1, " amount:", Math.min(clockwise, cclockwise));
 //    S   =   "one of your neighbors is"    ?W
 //            B   =   "one of your neighbors is"    ?P
 //            ?P  =>  "All of your neighbors are"   B
 //    OK  =>  "All of your neighbors are"   OK
-//    #?W  =>  "All of your neighbors are"   B
+//    #?W  =>  "All of your neighbors are"   B'
+    /**
+     * Internal state of the agents knowledge.
+     */
+    private BoardPiece[][] knowledgeBase;
+
+    /**
+     * Default constructor.
+     *
+     * @param rows the number of rows to build up KB
+     * @param cols the number of cols to build up KB
+     */
+    public InferenceAgent(final int rows, final int cols) {
+        knowledgeBase = new BoardPiece[rows][cols];
+    }
+
+    /**
+     * Provides a "next-move" for the provided current piece.
+     *
+     * @param bp the piece
+     * @return a piece to move from the specified {@param bp}
+     */
+    public BoardPiece ask(final BoardPiece bp) {
+
+        return null;
+    }
+
+    /**
+     * Updates the knowledge base with the pieces state.
+     *
+     * @param bp the piece
+     */
+    public void tell(final BoardPiece bp) {
+
+    }
 }
